6da2891586702a0c90c5012651738c51a8a0aa44
vanalex <apjorquera@gmail.com>
2022-06-22 09:56:10 +0200

    add other http verbs to the api

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -8,7 +8,9 @@ import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -25,17 +27,40 @@ class MessageResource(val service: MessageService){
 	@GetMapping
 	fun index(): List<Message> = service.findMessages()
 
+	@GetMapping("all")
+	fun listAllMessages(): List<Message> = service.findAllMessages()
+
 	@PostMapping
 	fun postMessage(@RequestBody message: Message){
 		service.post(message)
+	}
+
+	@DeleteMapping
+	fun deleteMessage(){
+		service.deleteMessages()
+	}
+
+	@DeleteMapping("id/{id}")
+	fun deleteMessage(@PathVariable("id") id: String){
+		service.deleteMessageById(id)
 	}
 }
 
 @Service
 class MessageService(val db: MessageRepository){
 	fun findMessages(): List<Message> = db.findMessages()
+
+	fun findAllMessages(): List<Message> = db.findAll()
 	fun post(message: Message){
 		db.save(message)
+	}
+
+	fun deleteMessages(){
+		db.deleteAll()
+	}
+
+	fun deleteMessageById(id :String){
+		db.deleteById(id)
 	}
 }
 
@@ -43,6 +68,9 @@ class MessageService(val db: MessageRepository){
 interface MessageRepository : CrudRepository<Message, String>{
 	@Query("select * from messages")
 	fun findMessages(): List<Message>
+	override fun findAll(): List<Message>
+
+	override fun deleteAll(): Unit
 }
 
 @Table("MESSAGES")
